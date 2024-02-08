4816e35eff664baba760e3dd2d39914ebfcb576a
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-02-09 03:28:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,14 @@ from fastapi.responses import JSONResponse
 app = FastAPI()
 @app.get("/status")
 async def prn_status(pnr: str):
-    return {"hello":"world"}
+    # Specify the URL of the website
+    url = f"https://www.confirmtkt.com/pnr-status/{pnr}"
+
+    # Send an HTTP request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return response.text
